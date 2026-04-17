64be17b57ef8c1711aba9a756424eafc349836cc
Laurish <daniel.skshipek@hotmail.com>
2026-04-17 13:34:06 +0200

    Add automatic tax to checkout session

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
         quantity: item.quantity,
       })),
       mode: "payment",
+      automatic_tax: { enabled: true },
       shipping_address_collection: {
         allowed_countries: ["SE"],
       },
